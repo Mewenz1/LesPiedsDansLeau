3f16196946cf49ccf353cd05e8a4a622942956bc
Mewen-Lucas <lucas.mewen1@gmail.com>
2021-09-29 17:48:59 +0200

    folders

--- app/src/main/java/com/example/lespiedsdansleau/IHM/MainActivity.java
+++ app/src/main/java/com/example/lespiedsdansleau/IHM/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.lespiedsdansleau;
+package com.example.lespiedsdansleau.IHM;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.lespiedsdansleau.R;
 
 public class MainActivity extends AppCompatActivity {
 
